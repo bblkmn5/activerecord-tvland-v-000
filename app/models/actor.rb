70dc7e5a0c8866d806ef5b603dea8bff49e0e4cd
Ben Blackman <bblkmn5@gmail.com>
2017-11-28 19:32:42 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,6 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    characters.map { |character| "#{character.name} - #{character.show.name}"  }
+    characters.map { |character| "#{character.name} - #{character.show.name}"}
   end
 end
